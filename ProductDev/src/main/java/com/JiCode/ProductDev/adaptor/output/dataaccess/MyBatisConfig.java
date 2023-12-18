a258a63610ccc6b3a0162118017e01b0c164a328
うさぎ <momoyama.sawa@gmail.com>
2023-12-18 21:08:42 +0800

    feat: 连接数据源到MyBatis

--- ProductDev/src/main/java/com/JiCode/ProductDev/adaptor/output/dataaccess/MyBatisConfig.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/adaptor/output/dataaccess/MyBatisConfig.java
@@ -10,21 +10,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-// @Configuration
-// @MapperScan(basePackages = "com.JiCode.ProductDev") // 指定 MyBatis
-// Mapper接口的包路径
-// public class MyBatisConfig {
+@Configuration
+@MapperScan(basePackages = "com.JiCode.ProductDev.adaptor.output.dataaccess.mappers")
+public class MyBatisConfig {
 
-// @Autowired
-// private DataSource dataSource; // 这里会自动注入 MSDataSourceConf 中定义的数据源
+    @Autowired
+    private DataSource dataSource; // 这里会自动注入 MSDataSourceConf 中定义的数据源
 
-// @Bean
-// public SqlSessionFactory sqlSessionFactory() throws Exception {
-// SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-// factoryBean.setDataSource(dataSource); // 设置数据源
-// factoryBean.setMapperLocations(
-// new
-// PathMatchingResourcePatternResolver().getResources("classpath*:com/JiCode/ProductDev/**/*.xml"));
-// return factoryBean.getObject();
-// }
-// }
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        System.out.println("\033[31m" + "这是红色的文字" + "\033[0m");
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource); // 设置数据源
+        factoryBean.setTypeAliasesPackage("com.JiCode.ProductDev.adaptor.output.dataaccess.DBModels");
+        System.out.println("\033[31m" + "这是红色的文字" + "\033[0m");
+        factoryBean.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath*:com/JiCode/ProductDev/**/*.xml"));
+        return factoryBean.getObject();
+    }
+}

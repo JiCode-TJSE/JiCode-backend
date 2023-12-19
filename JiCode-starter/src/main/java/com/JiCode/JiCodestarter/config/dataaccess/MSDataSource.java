bb717906d16149a81855a55e5adef0b8a36d933c
うさぎ <momoyama.sawa@gmail.com>
2023-12-19 11:40:06 +0800

    feat: Datasource

---
+++ JiCode-starter/src/main/java/com/JiCode/JiCodestarter/config/dataaccess/MSDataSource.java
@@ -0,0 +1,48 @@
+package com.JiCode.JiCodestarter.config.dataaccess;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class MSDataSource extends AbstractRoutingDataSource {
+
+    // 从库的 Key 列表
+    private List<Object> slaveKeys;
+
+    // 从库 key 列表的索引，用原子 int 是为了保证多线程数据安全
+    private AtomicInteger index = new AtomicInteger(0);
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+
+        // 当前业务的读写标识，之后写一个判断吧，可以根据当前请求的url，先写死
+        // 当前线程的主从标识
+        Boolean master = MSDataSourceMarker.get();
+
+        if (master == null || master || this.slaveKeys.isEmpty()) {
+            // 主库，返回 null，使用默认数据源
+            return null;
+        }
+
+        // 从库，从 slaveKeys 中选择一个 Key
+        int index = this.index.getAndIncrement() % this.slaveKeys.size();
+
+        // 保证 int 数值的安全
+        if (this.index.get() > 9999999) {
+            this.index.set(0);
+        }
+
+        Object key = slaveKeys.get(index);
+
+        return key;
+    }
+
+    public List<Object> getSlaveKeys() {
+        return slaveKeys;
+    }
+
+    public void setSlaveKeys(List<Object> slaveKeys) {
+        this.slaveKeys = slaveKeys;
+    }
+}
--- JiCode-starter/src/test/java/com/JiCode/JiCodestarter/JiCodeStarterApplicationTests.java
+++ JiCode-starter/src/test/java/com/JiCode/JiCodestarter/JiCodeStarterApplicationTests.java
@@ -3,11 +3,31 @@ package com.JiCode.JiCodestarter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class JiCodeStarterApplicationTests {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import com.JiCode.JiCodestarter.service.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class JiCodeStarterApplicationTests {
+
+	static final Logger log = LoggerFactory.getLogger(JiCodeStarterApplicationTests.class);
+
+	@Autowired
+	TestService testService;
 
 	@Test
-	void contextLoads() {
+	public void test() throws Exception {
+
+		// 连续4次读
+		log.info("read={}", this.testService.read());
+		log.info("read={}", this.testService.read());
+		log.info("read={}", this.testService.read());
+		log.info("read={}", this.testService.read());
+
+		// 写
+		log.info("write={}", this.testService.write());
 	}
 
 }

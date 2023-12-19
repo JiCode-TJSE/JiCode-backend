b8e178e28105d2915ea1c4e8f9f5dd6dde1c4424
Laurant_NG <2496803478@qq.com>
2023-12-19 17:13:14 +0800

    feat: change aggregation factory

--- ProductDev/src/main/java/com/JiCode/ProductDev/domain/model/ScheduleAggregation.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/domain/model/ScheduleAggregation.java
@@ -3,12 +3,14 @@ package com.JiCode.ProductDev.domain.model;
 import com.JiCode.ProductDev.adaptor.output.dataaccess.DBModels.Schedule;
 import com.JiCode.ProductDev.domain.repository.ScheduleRepository;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
+@NoArgsConstructor
 public class ScheduleAggregation {
     @Autowired
     ScheduleRepository scheduleRepository;
@@ -19,12 +21,14 @@ public class ScheduleAggregation {
     public Integer remainWorkhour;
     public float progress;
 
-    public ScheduleAggregation() {
-    }
-
-    static public ScheduleAggregation createSchdule(Schedule schedule){
+    // 工厂模式
+    static public ScheduleAggregation createSchedule(String id, int estimatedWorkhour, int actualWorkhour,int remainWorkhour, float progress){
         ScheduleAggregation scheduleAggregation = new ScheduleAggregation();
-        BeanUtils.copyProperties(schedule, scheduleAggregation);
+        scheduleAggregation.id = id;
+        scheduleAggregation.estimatedWorkhour = estimatedWorkhour;
+        scheduleAggregation.actualWorkhour = actualWorkhour;
+        scheduleAggregation.remainWorkhour = remainWorkhour;
+        scheduleAggregation.progress = progress;
         System.out.println(scheduleAggregation);
         return scheduleAggregation;
     }

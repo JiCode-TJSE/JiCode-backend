a6a7f0f17ba1ac2f4a9712f073bf5b8dd5fcc37c
うさぎ <momoyama.sawa@gmail.com>
2023-12-19 14:22:09 +0800

    perf: JiCode-starter

--- ProductDev/src/main/java/com/JiCode/ProductDev/application/ScheduleApplication.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/application/ScheduleApplication.java
@@ -7,6 +7,7 @@ import com.JiCode.ProductDev.domain.repository.ScheduleRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ScheduleApplication {
@@ -16,7 +17,8 @@ public class ScheduleApplication {
     @Autowired
     ScheduleRepository scheduleRepository;
 
-    public SelectScheduleByIdDto selectById(String id){
+    @Transactional(readOnly = true)
+    public SelectScheduleByIdDto selectById(String id) {
         ScheduleAggregation sa = scheduleRepository.selectById(id);
         SelectScheduleByIdDto selectScheduleByIdDto = new SelectScheduleByIdDto();
         BeanUtils.copyProperties(sa, selectScheduleByIdDto);

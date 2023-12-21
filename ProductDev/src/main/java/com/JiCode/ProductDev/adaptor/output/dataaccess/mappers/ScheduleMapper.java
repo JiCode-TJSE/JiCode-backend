535468f008cdf383f373320ae0db20fef37c68c5
Laurant_NG <2496803478@qq.com>
2023-12-21 08:55:31 +0800

    feat: add mapperscan

--- ProductDev/src/main/java/com/JiCode/ProductDev/adaptor/output/dataaccess/mappers/ScheduleMapper.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/adaptor/output/dataaccess/mappers/ScheduleMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-@Mapper
+
 public interface ScheduleMapper {
     long countByExample(ScheduleExample example);
 

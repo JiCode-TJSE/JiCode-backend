47c0b41995e4db124c70cf3ec10bbe39e4bb6e3b
sty <2969627331@qq.com>
2024-01-01 00:24:36 +0800

    fix: service

--- ProductDev/src/main/java/com/JiCode/ProductDev/domain/service/correlation/impl/RelateServiceImpl.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/domain/service/correlation/impl/RelateServiceImpl.java
@@ -1,6 +1,7 @@
 package com.JiCode.ProductDev.domain.service.correlation.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.JiCode.ProductDev.common.RelateItemTypeEnum;
 import com.JiCode.ProductDev.domain.bo.RelateBo;
@@ -10,6 +11,7 @@ import com.JiCode.ProductDev.domain.repository.SprintRepository;
 import com.JiCode.ProductDev.domain.service.correlation.RelateService;
 import com.JiCode.ProductDev.domain.service.correlation.strategy.RelateStrategy;
 
+@Service
 public class RelateServiceImpl implements RelateService {
     @Autowired
     RelateStrategy relateStrategy;
--- ProductDev/src/main/java/com/JiCode/ProductDev/domain/service/correlation/strategy/SprintReleaseStrategy.java
+++ ProductDev/src/main/java/com/JiCode/ProductDev/domain/service/correlation/strategy/SprintReleaseStrategy.java
@@ -1,8 +1,11 @@
 package com.JiCode.ProductDev.domain.service.correlation.strategy;
 
+import org.springframework.stereotype.Service;
+
 import com.JiCode.ProductDev.common.RelateItemTypeEnum;
 import com.JiCode.ProductDev.domain.bo.RelateBo;
 
+@Service
 public final class SprintReleaseStrategy extends RelateStrategy {
     @Override
     public boolean testType(RelateBo item1, RelateBo item2)

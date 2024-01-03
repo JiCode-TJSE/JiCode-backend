c096e5a40d68c3fa90754b28a4c4d0e23c260b2e
Laurant_NG <wuhang1008@163.com>
2024-01-04 00:51:05 +0800

    feat: add backstage branch

---
+++ Backstage/src/main/java/com/JiCode/Backstage/BackstageApplication.java
@@ -0,0 +1,13 @@
+package com.JiCode.Backstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackstageApplication.class, args);
+	}
+
+}
